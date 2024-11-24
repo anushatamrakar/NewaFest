4cea08074eac578d3bfb8a5156da28fce0102092
anushatamrakar <anusha.tamrakar@patancollege.edu.np>
2024-11-24 19:42:06 +0545

    branch created

---
+++ lib/views/auth/user_registration.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:newafest/widgets/custom_banner.dart';
+import 'package:newafest/widgets/custom_button.dart';
+class UserRegistration extends StatelessWidget {
+  const UserRegistration({super.key});
+  final border = const OutlineInputBorder(
+    borderSide: BorderSide(
+      width: 1.5,
+      style: BorderStyle.solid,
+      color: Colors.grey,
+    ),
+    borderRadius: BorderRadius.all(Radius.circular(5)),
+  );
+
+  Widget name() {
+    return TextField(
+      style: const TextStyle(
+        fontSize: 15,
+        fontFamily: "Poppins",
+        fontWeight: FontWeight.w400,
+      ),
+      decoration: InputDecoration(
+        labelText: "Fullname",
+        filled: true,
+        fillColor: Colors.white,
+        focusedBorder: border,
+        enabledBorder: border,
+        labelStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 15,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  //initialising password text field
+  Widget number() {
+    return TextField(
+      style: const TextStyle(
+        fontSize: 15,
+        fontFamily: "Poppins",
+        fontWeight: FontWeight.w400,
+      ),
+      decoration: InputDecoration(
+        labelText: "Phone Number",
+        filled: true,
+        fillColor: Colors.white,
+        focusedBorder: border,
+        enabledBorder: border,
+        labelStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 15,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+  Widget _email() {
+    return TextField(
+      style: const TextStyle(
+        fontSize: 15,
+        fontFamily: "Poppins",
+        fontWeight: FontWeight.w400,
+      ),
+      decoration: InputDecoration(
+        labelText: "Email or Phone",
+        filled: true,
+        fillColor: Colors.white,
+        focusedBorder: border,
+        enabledBorder: border,
+        labelStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 15,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  //initialising password text field
+  Widget _password() {
+    return TextField(
+      style: const TextStyle(
+        fontSize: 15,
+        fontFamily: "Poppins",
+        fontWeight: FontWeight.w400,
+      ),
+      decoration: InputDecoration(
+        labelText: "Password",
+        filled: true,
+        fillColor: Colors.white,
+        suffixIcon: const Icon(Icons.visibility_off_outlined),
+        focusedBorder: border,
+        enabledBorder: border,
+        labelStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 15,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            const CustomBanner(text1: "Sign up", text2: "Create your own account"),
+            Padding(padding: EdgeInsets.all(28),
+            child: Column(
+                children: [
+                  name(),
+                  const SizedBox(height:15),
+                  number(),
+                  const SizedBox(height:15),
+                  _email(),
+                  const SizedBox(height: 15),
+                  _password(),
+              ],
+            )),
+            const CustomButton(txt1: "Register"),
+          ],
+        ),
+      ),
+    );
+  }
+}
